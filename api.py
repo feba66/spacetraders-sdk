fdb8848775b6aca490152ef395327c3302f7b58a
Felix Baumkötter <fb570244@fh-muenster.de>
2022-12-25 23:26:50 +0100

    Default and Agents API done others partly

--- api.py
+++ api.py
@@ -11,11 +11,179 @@ from enum import Enum
 
 class ErrorCodes(Enum):
     WAYPOINT_ALREADY_CHARTED = 4230
+
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
+
+@dataclass
+class Agent:
+    accountId:str
+    symbol:str
+    headquarters:str
+    credits:int
+
+    def __init__(self,data) -> None:
+        self.accountId=data["accountId"]
+        self.symbol=data["symbol"]
+        self.headquarters=data["headquarters"]
+        self.credits=data["credits"].as_int_oapg
+class ContractType(Enum):
+    PROCUREMENT="PROCUREMENT"
+    TRANSPORT="TRANSPORT"
+    SHUTTLE="SHUTTLE"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
+@dataclass
+class ContractPayment:
+    onFulfilled:int
+    onAccepted:int
+    def __init__(self,data) -> None:
+        self.onFulfilled=data["onFulfilled"].as_int_oapg
+        self.onAccepted=data["onAccepted"].as_int_oapg
+@dataclass
+class ContractDeliverGood:
+    tradeSymbol:str
+    unitsRequired:int
+    destinationSymbol:str
+    unitsFulfilled:int
+
+    def __init__(self,data) -> None:
+        self.tradeSymbol=data["tradeSymbol"]
+        self.unitsRequired=data["unitsRequired"].as_int_oapg
+        self.destinationSymbol=data["destinationSymbol"]
+        self.unitsFulfilled=data["unitsFulfilled"].as_int_oapg
+@dataclass
+class ContractTerms:
+    payment:ContractPayment
+    deadline:str
+    deliver:Optional[list[ContractDeliverGood]]
+@dataclass
+class Contract:
+    terms:ContractTerms
+    factionSymbol:str
+    fulfilled:bool
+    accepted:bool
+    expiration:str
+    id:str
+    type:ContractType
+
+    def __init__(self,data) -> None:
+        self.terms = ContractTerms(ContractPayment(data["terms"]["payment"]),data["terms"]["deadline"],[ContractDeliverGood(d) for d in data["terms"]["deliver"]])
+        self.id = data["id"]
+        self.fulfilled = data["fulfilled"].is_true_oapg()
+        self.accepted = data["accepted"].is_true_oapg()
+        self.expiration = data["expiration"]
+        self.factionSymbol = data["factionSymbol"]
+        self.type = ContractType[data["type"]]
+class FactionTraitSymbol(Enum):
+    BUREAUCRATIC="BUREAUCRATIC"
+    SECRETIVE="SECRETIVE"
+    CAPITALISTIC="CAPITALISTIC"
+    INDUSTRIOUS="INDUSTRIOUS"
+    PEACEFUL="PEACEFUL"
+    DISTRUSTFUL="DISTRUSTFUL"
+    WELCOMING="WELCOMING"
+    ANARCHIST="ANARCHIST"
+    CONFLICTED="CONFLICTED"
+    AUTHORITARIAN="AUTHORITARIAN"
+    OLIGARCHICAL="OLIGARCHICAL"
+    DYNASTIC="DYNASTIC"
+    DEMOCRACTIC="DEMOCRACTIC"
+    DECENTRALIZED="DECENTRALIZED"
+    SMUGGLERS="SMUGGLERS"
+    SCAVENGERS="SCAVENGERS"
+    REBELLIOUS="REBELLIOUS"
+    EXILES="EXILES"
+    PIRATES="PIRATES"
+    RAIDERS="RAIDERS"
+    CLAN="CLAN"
+    GUILD="GUILD"
+    DOMINION="DOMINION"
+    FRINGE="FRINGE"
+    FORSAKEN="FORSAKEN"
+    ISOLATED="ISOLATED"
+    LOCALIZED="LOCALIZED"
+    ESTABLISHED="ESTABLISHED"
+    NOTABLE="NOTABLE"
+    DOMINANT="DOMINANT"
+    INESCAPABLE="INESCAPABLE"
+    INNOVATIVE="INNOVATIVE"
+    BOLD="LD"
+    VISIONARY="VISIONARY"
+    CURIOUS="CURIOUS"
+    DARING="DARING"
+    EXPLORATORY="EXPLORATORY"
+    RESOURCEFUL="RESOURCEFUL"
+    FLEXIBLE="FLEXIBLE"
+    COOPERATIVE="COOPERATIVE"
+    UNITED="UNITED"
+    STRATEGIC="STRATEGIC"
+    INTELLIGENT="INTELLIGENT"
+    RESEARCH_FOCUSED="RESEARCH_FOCUSED"
+    COLLABORATIVE="COLLABORATIVE"
+    PROGRESSIVE="PROGRESSIVE"
+    MILITARISTIC="MILITARISTIC"
+    TECHNOLOGICALLY_ADVANCED="TECHNOLOGICALLY_ADVANCED"
+    AGGRESSIVE="AGGRESSIVE"
+    IMPERIALISTIC="IMPERIALISTIC"
+    TREASURE_HUNTERS="TREASURE_HUNTERS"
+    DEXTEROUS="DEXTEROUS"
+    UNPREDICTABLE="UNPREDICTABLE"
+    BRUTAL="BRUTAL"
+    FLEETING="FLEETING"
+    ADAPTABLE="ADAPTABLE"
+    SELF_SUFFICIENT="SELF_SUFFICIENT"
+    DEFENSIVE="DEFENSIVE"
+    PROUD="PROUD"
+    DIVERSE="DIVERSE"
+    INDEPENDENT="INDEPENDENT"
+    SELF_INTERESTED="SELF_INTERESTED"
+    FRAGMENTED="FRAGMENTED"
+    COMMERCIAL="COMMERCIAL"
+    FREE_MARKETS="FREE_MARKETS"
+    ENTREPRENEURIAL="ENTREPRENEURIAL"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
+@dataclass
+class FactionTrait:
+    symbol:FactionTraitSymbol
+    name:str
+    description:str
+    def __init__(self,data) -> None:
+        self.name=data["name"]
+        self.description=data["description"]
+        self.symbol = FactionTraitSymbol[data["symbol"]]
+@dataclass
+class Faction:
+    symbol:str
+    headquarters:str
+    traits:list[FactionTrait]
+    name:str
+    description:str
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.headquarters=data["headquarters"]
+        self.name=data["name"]
+        self.description=data["description"]
+        self.traits=[FactionTrait(t) for t in data["traits"]]
 @dataclass
 class ShipRequirements():
     power:int
     crew:int
     slots:int
+    def __init__(self,data) -> None:
+        self.power=data["power"].as_int_oapg if "power" in data else None
+        self.crew=data["crew"].as_int_oapg if "crew" in data else None
+        self.slots=data["slots"].as_int_oapg if "slots" in data else None
 @dataclass
 class ShipEngine():
     symbol: str
@@ -24,6 +192,13 @@ class ShipEngine():
     description:str
     speed: int
     condition: int
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.requirements=ShipRequirements(data["requirements"])
+        self.name=data["name"]
+        self.description=data["description"]
+        self.speed=data["speed"].as_int_oapg
+        self.condition=data["condition"].as_int_oapg
 @dataclass
 class ShipReactor():
     symbol:str
@@ -32,21 +207,40 @@ class ShipReactor():
     description:str
     powerOutput:str
     condition:int
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.requirements=ShipRequirements(data["requirements"])
+        self.name=data["name"]
+        self.description=data["description"]
+        self.powerOutput=data["powerOutput"]
+        self.condition=data["condition"].as_int_oapg
 @dataclass
 class Consumed():
     amount: int
     timestamp: str
+    def __init__(self,data) -> None:
+        self.amount=data["amount"].as_int_oapg
+        self.timestamp=data["timestamp"]
 @dataclass
 class ShipFuel():
     current: int
     capacity: int
     consumed: Consumed
+    def __init__(self,data) -> None:
+        self.current=data["current"].as_int_oapg
+        self.capacity=data["capacity"].as_int_oapg
+        self.consumed=Consumed(data["consumed"])
 @dataclass
 class ShipCargoItem:
     symbol:str
     name:str
     description:str
     units:int
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.name=data["name"]
+        self.description=data["description"]
+        self.units=data["units"].as_int_oapg
 @dataclass
 class ShipModule:
     symbol:str
@@ -55,6 +249,13 @@ class ShipModule:
     capacity:Optional[int]
     range:Optional[int]
     description:Optional[str]
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.requirements=ShipRequirements(data["requirements"])
+        self.name=data["name"]
+        self.capacity=data["capacity"].as_int_oapg if "capacity" in data else None
+        self.range=data["range"].as_int_oapg if "range" in data else None
+        self.description=data["description"] if "description" in data else None
 @dataclass
 class ShipMount:
     symbol:str
@@ -62,11 +263,21 @@ class ShipMount:
     name:str
     description:Optional[str]
     strength:Optional[int]
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.requirements=ShipRequirements(data["requirements"])
+        self.name=data["name"]
+        self.description=data["description"] if "description" in data else None
+        self.strength=data["strength"] if "description" in data else None
 @dataclass
 class ShipCargo:
     units: int
     inventory: list[ShipCargoItem]
     capacity: int
+    def __init__(self,data) -> None:
+        self.units=data["units"].as_int_oapg
+        self.inventory=[ShipCargoItem(x) for x in data["inventory"]]
+        self.capacity=data["capacity"].as_int_oapg
 @dataclass
 class ShipFrame:
     symbol:str
@@ -77,6 +288,15 @@ class ShipFrame:
     description:str
     mountingPoints:int
     condition:Optional[int]
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.moduleSlots=data["moduleSlots"].as_int_oapg
+        self.requirements=ShipRequirements(data["requirements"])
+        self.fuelCapacity=data["fuelCapacity"].as_int_oapg
+        self.name=data["name"]
+        self.description=data["description"]
+        self.mountingPoints=data["mountingPoints"].as_int_oapg
+        self.condition=data["condition"].as_int_oapg if "condition" in data else None
 @dataclass
 class ShipCrew:
     # The amount of credits per crew member paid per hour. Wages are paid when a ship docks at a civilized waypoint.
@@ -86,11 +306,22 @@ class ShipCrew:
     morale:int
     required:int
     capacity:int
+    def __init__(self,data) -> None:
+        self.wages=data["wages"].as_int_oapg
+        self.current=data["current"].as_int_oapg
+        self.rotation=data["rotation"]
+        self.morale=data["morale"].as_int_oapg
+        self.required=data["required"].as_int_oapg
+        self.capacity=data["capacity"].as_int_oapg
 @dataclass
 class ShipRegistration:
     role:str
     name:str
     factionSymbol:Optional[str]
+    def __init__(self,data) -> None:
+        self.role=data["role"]
+        self.name=data["name"]
+        self.factionSymbol=data["factionSymbol"] if "factionSymbol" in data else None
 @dataclass
 class ShipNavRouteWaypoint:
     symbol:str
@@ -98,27 +329,54 @@ class ShipNavRouteWaypoint:
     x:int
     y:int
     type:str
+    def __init__(self,data) -> None:
+        self.symbol=data["symbol"]
+        self.systemSymbol=data["systemSymbol"]
+        self.x=data["x"].as_int_oapg
+        self.y=data["y"].as_int_oapg
+        self.type=data["type"]
 @dataclass
 class ShipNavRoute:
     arrival:str
     destination:ShipNavRouteWaypoint
     departure:ShipNavRouteWaypoint
+    def __init__(self,data) -> None:
+        self.arrival = data["arrival"]
+        self.destination = ShipNavRouteWaypoint(data["destination"])
+        self.departure = ShipNavRouteWaypoint(data["departure"])
 class ShipNavFlightMode(Enum):
     DRIFT="DRIFT"
     STEALTH="STEALTH"
     CRUISE="CRUISE"
     BURN="BURN"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
 class ShipNavStatus(Enum):
     IN_TRANSIT="IN_TRANSIT"
     IN_ORBIT="IN_ORBIT"
     DOCKED="DOCKED"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
 @dataclass
 class ShipNav:
     route: ShipNavRoute
     systemSymbol:str
-    waypointsSymbol:str
+    waypointSymbol:str
     flightMode:ShipNavFlightMode
     status:ShipNavStatus
+
+    def __init__(self,data) -> None:
+        self.route=ShipNavRoute(data["route"])
+        self.systemSymbol=data["systemSymbol"]
+        self.waypointSymbol=data["waypointSymbol"]
+        self.flightMode=ShipNavFlightMode[data["flightMode"]]
+        self.status=ShipNavStatus[data["status"]]
 @dataclass
 class Ship():
     nav: ShipNav
@@ -131,6 +389,19 @@ class Ship():
     modules: list[ShipModule]
     crew: ShipCrew
     frame: ShipFrame
+
+    def __init__(self,data) -> None:
+        self.nav=ShipNav(data["nav"])
+        self.engine=ShipEngine(data["engine"])
+        self.fuel=ShipFuel(data["fuel"])
+        self.reactor=ShipReactor(data["reactor"])
+        self.registration=ShipRegistration(data["registration"])
+        self.cargo=ShipCargo(data["cargo"])
+        self.crew=ShipCrew(data["crew"])
+        self.frame=ShipFrame(data["frame"])
+        self.mounts=[ShipMount(x) for x in data["mounts"]]
+        self.modules=[ShipModule(x) for x in data["modules"]]
+
 class SystemType(Enum):
     NEUTRON_STAR="NEUTRON_STAR"
     RED_STAR="RED_STAR"
@@ -142,6 +413,11 @@ class SystemType(Enum):
     HYPERGIANT="HYPERGIANT"
     NEBULA="NEBULA"
     UNSTABLE="UNSTABLE"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
 class WaypointType(Enum):
     PLANET="PLANET"
     GAS_GIANT="GAS_GIANT"
@@ -152,6 +428,11 @@ class WaypointType(Enum):
     NEBULA="NEBULA"
     DEBRIS_FIELD="DEBRIS_FIELD"
     GRAVITY_WELL="GRAVITY_WELL"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
 @dataclass
 class SystemWaypoint:
     symbol:str
@@ -255,6 +536,12 @@ class WaypointTraitSymbols(Enum):
     TEMPERATE="TEMPERATE"
     JUNGLE="JUNGLE"
     OCEAN="OCEAN"
+    STRIPPED="STRIPPED"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    def __str__(self) -> str:
+        return self.name
 @dataclass
 class WaypointTrait:
     symbol:WaypointTraitSymbols
@@ -325,12 +612,16 @@ class SpaceTraders:
     api_systems: systems_api.SystemsApi
 
     systems: list[System]
+    agent:Agent
 
     def __init__(self) -> None:
 
         configuration = openapi_client.Configuration()
-        with open("acc.txt", "r") as f:
-            configuration.access_token = f.readline().replace("\n", "")
+        try:
+            with open("spacetraders-sdk/acc.txt", "r") as f:
+                configuration.access_token = f.readline().replace("\n", "")
+        except:
+            pass
         self.api_client = openapi_client.ApiClient(configuration)
 
         self.api_agents = agents_api.AgentsApi(self.api_client)
@@ -340,24 +631,46 @@ class SpaceTraders:
         self.api_fleet = fleet_api.FleetApi(self.api_client)
         self.api_systems = systems_api.SystemsApi(self.api_client)
 
-    def register(self):
-        # Create an instance of the API class
+    # region DefaultApi
+    def register(self,name,faction):
         try:
-            r = self.def_api.register(body={"symbol": "febat3st", "faction": "ASTRO"})
-            pprint(r.body)
-            pprint(r.response.data)
+            r = self.def_api.register(body={"symbol": name, "faction": faction})
+
+            token = r.body["data"]["token"]
+            print(token)
+            with open("spacetraders-sdk/acc.txt","a") as f:
+                f.write(token+"\n")
+
+            agent = Agent(r.body["data"]["agent"])
+            contract = Contract(r.body["data"]["contract"])
+            factionc = Faction(r.body["data"]["faction"])
+            ship = Ship(r.body["data"]["ship"])
+            pprint(agent)
+            pprint(contract)
+            pprint(factionc)
+            pprint(ship)
+            # pprint(r.response.data)
             return r
         except openapi_client.ApiException as e:
             pprint(e)
-
+    # endregion
     # region AgentsApi
     def get_agent(self):
         try:
             api_response = self.api_agents.get_my_agent()
-            pprint(api_response)
-            return api_response
+            agent = Agent(api_response.body["data"])
+            self.agent = agent
+            return  agent
         except openapi_client.ApiException as e:
             print("Exception when calling AgentsApi->get_my_agent: %s\n" % e)
+    # endregion
+    # region FactionsApi
+    def get_factions(self):
+        try:
+            api_response = self.api_factions.get_factions()
+            return [Faction(f) for f in api_response.body["data"]]
+        except openapi_client.ApiException as e:
+            print("Exception when calling FactionsApi->get_factions: %s\n" % e)
     # endregion
     # region ContactsApi
 
@@ -379,12 +692,16 @@ class SpaceTraders:
         except openapi_client.ApiException as e:
             print("Exception when calling ContractsApi->deliver_contract: %s\n" % e)
 
+    def get_contract(self,contractId):
+        try:
+            api_response = self.api_contracts.get_contract(path_params = {"contractId": contractId})
+            return Contract(api_response.body["data"])
+        except openapi_client.ApiException as e:
+            print("Exception when calling ContractsApi->get_contract: %s\n" % e)
     def get_contracts(self):
-
         try:
             api_response = self.api_contracts.get_contracts()
-            pprint(api_response)
-            return api_response
+            return [Contract(c) for c in api_response.body["data"]]
         except openapi_client.ApiException as e:
             print("Exception when calling ContractsApi->get_contracts: %s\n" % e)
     # endregion
@@ -420,13 +737,16 @@ class SpaceTraders:
             print("Exception when calling ContractsApi->get_contracts: %s\n" % e)
     # endregion
     # region SystemsApi
-    def get_systems(self) -> list[System]:#done
+    def get_systems(self) -> list[System]:
         """Gets all systems
         :return: A list of all Systems
         """
         try:
             r = self.api_systems.get_systems()
             systems = []
+            with open("systems.json","w") as f:
+                f.write(str(r.response.data))
+            print(r.body["meta"])
             for s in r.body["data"]:
                 system = System(s)
                 systems.append(system)
@@ -434,7 +754,7 @@ class SpaceTraders:
             return systems
         except openapi_client.ApiException as e:
             print("Exception when calling SystemApi->get_systems: %s\n" % e)
-    def get_system(self,systemSymbol):#done
+    def get_system(self,systemSymbol):
         """Gets the system information 
         :param systemSymbol: System name
         :return: the system object
@@ -471,14 +791,21 @@ class SpaceTraders:
 
 if __name__ == "__main__":
     st = SpaceTraders()
-    st.get_agent()
-    # st.get_contracts()
+
+    # st.register("test0003","COSMIC")
+    pprint(st.get_factions())
+
+    # exit()
+    # pprint(st.get_agent())
+
+    # pprint(st.get_contract("clc3oyf4l000ss60j6lzyx2mv"))
+    exit()
     # contract = "clbw3quox000ss60ju7qe7a8b"
     # st.accept_contract(contract)
     # st.get_my_ships()
-    ship = "FEBAT3ST-1"
+    # ship = "FEBAT3ST-1"
     # st.chart(ship)
-    system = "X1-PH88"
+    system = "X1-UV97"
     shipyards = []
     markets=[]
     s = st.get_system(system)
@@ -491,18 +818,20 @@ if __name__ == "__main__":
             if t.symbol==WaypointTraitSymbols.MARKETPLACE:
                 markets.append(w.symbol)
             print(" "+t.name)
+    print("== Shipyards ==")
     for shipyard in shipyards:
         print(shipyard)
         sy = st.get_shipyard(system,shipyard).body["data"]
         # print(sy)
         for t in sy["shipTypes"]:
             print(" "+t["type"])
+    print("== Markets ==")
     for market in markets:
         print(market)
         m = st.get_market(system,market)
         print(m)
-    #         # ns = st.purchase_ship(shipyard,t["type"])
-    #         # print(ns)
+            # ns = st.purchase_ship(shipyard,t["type"])
+            # print(ns)
     # st.get_systems()
     # for s in st.systems:
     #     print(s)
