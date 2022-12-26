fe71866b46f1cf3815b67f33621f4d810da8c38f
Felix Baumkötter <fb570244@fh-muenster.de>
2022-12-27 00:20:43 +0100

    client new

--- client.py
+++ client.py
@@ -15,24 +15,27 @@ ships = st.get_my_ships()
 
 sg.theme("dark")
 
-agent_info = [[sg.Text(agent.symbol)], [sg.Text("Credits: "+str(credits))]]
-ship_infos = []
-print()
-for s in ships:
-    #,sg.ProgressBar(s.crew.capacity,"h",(20,20),key="-crewBar-")
-    ship_infos.append([sg.Frame(s.symbol+" "+s.registration.role, [
-        [sg.Text("nav: "+str(s.nav.status)+"\n- Location: "+s.nav.waypointSymbol+"\n- Mode: "+str(s.nav.flightMode))],
-        [sg.Text("crew: "+str(s.crew.current)+"/"+str(s.crew.capacity)+" \nreq: "+str(s.crew.required)
-            +"\n- Rotation: "+str(s.crew.rotation)+"\n- Morale: "+str(s.crew.morale)+"\n- Wages: "+str(s.crew.wages))] if s.crew.capacity >0 else [],
-        [sg.Text("Fuel: "+str(s.fuel.current)+"/"+str(s.fuel.capacity))],
-        [sg.Text("Frame: "+str(s.frame.name)+"")]
-        ])
-    ])
-layout = [[sg.Frame("Agent", agent_info)], [sg.Frame("Ships", ship_infos)], [sg.Button("OK")]]
-window = sg.Window(title="Main Window", layout=layout, margins=(10, 10))
-window.read(0)
+# agent_info = [[sg.Text(agent.symbol)], [sg.Text("Credits: "+str(credits))]]
+# ship_infos = []
+# print()
+# for s in ships:
+#     #,sg.ProgressBar(s.crew.capacity,"h",(20,20),key="-crewBar-")
+#     ship_infos.append([sg.Frame(s.symbol+" "+s.registration.role, [
+#         [sg.Text("nav: "+str(s.nav.status)+"\n- Location: "+s.nav.waypointSymbol+"\n- Mode: "+str(s.nav.flightMode))],
+#         [sg.Text("crew: "+str(s.crew.current)+"/"+str(s.crew.capacity)+" \nreq: "+str(s.crew.required)
+#             +"\n- Rotation: "+str(s.crew.rotation)+"\n- Morale: "+str(s.crew.morale)+"\n- Wages: "+str(s.crew.wages))] if s.crew.capacity >0 else [],
+#         [sg.Text("Fuel: "+str(s.fuel.current)+"/"+str(s.fuel.capacity))],
+#         [sg.Text("Frame: "+str(s.frame.name)+"")]
+#         ])
+#     ])
+# layout = [[sg.Frame("Agent", agent_info)], [sg.Frame("Ships", ship_infos)], [sg.Button("OK")]]
+# window = sg.Window(title="Main Window", layout=layout, margins=(10, 10))
+# window.read(0)
 # window.find_element("-crewBar-").update_bar(s.crew.current)
-
+register_layout = [[]]
+login_layout = [[]]
+layout = [[sg.Frame("Register", register_layout)], [sg.Frame("Login", login_layout)]]
+window = sg.Window(title="Main Window", layout=layout, margins=(10, 10))
 while True:
     # window.-crewBar-.update_bar(s.crew.current)
 
@@ -42,3 +45,28 @@ while True:
         break
 
 window.close()
+
+
+
+
+
+
+
+
+"""
+1. Login/register
+register: textinput name
+          dropdown faction
+          checkbox keeploggedin
+login: textinput token
+        maybe dropdown with already saved tokens/usernames
+
+db: table users
+columns: username, token
+
+"""
+
+
+
+
+
